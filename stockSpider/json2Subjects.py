55b2cf2628dc8f5aa10bdf7d9ac3bc3587ccd24d
nikytwo <nikytwo@gmail.com>
2017-06-13 09:03:39 +0800

    feat: json 转对象

---
+++ stockSpider/json2Subjects.py
@@ -0,0 +1,36 @@
+import json
+
+
+class Subject:
+    def __init__(self):
+        self.date = ''
+        self.name = ''
+        self.value = ''
+
+
+if __name__ == '__main__':
+
+    # json to Subject class object
+    with open('../finance-600172.json') as json_file:
+        datas = json.load(json_file)
+        finance = {}
+        for k, v in datas.items():
+            if ('name' == k):
+                continue
+            names = v['title']
+            d = v['simple']
+            dates = d[0]
+            subjects = []
+            for x in range(len(dates)):
+                date = dates[x]
+                for i in range(1, len(names)):
+                    subject = Subject()
+                    subject.date = date
+                    subject.name = names[i][0]
+                    subject.value = d[i][x]
+                    subjects.append(subject)
+
+            finance[k] = subjects
+
+        print datas
+        print subjects

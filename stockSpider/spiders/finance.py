d324993573732ccd843f03fa47af47a151494bfc
nikytwo <nikytwo@gmail.com>
2017-07-10 15:00:02 +0800

    feat: 格式化财务数据，按日期、科目输出

--- stockSpider/spiders/finance.py
+++ stockSpider/spiders/finance.py
@@ -8,7 +8,7 @@ from stockSpider.items import FinanceItem
 
 class FinanceSpider(scrapy.Spider):
     name = "finance"
-    keys = []
+    keys = ['num']
     stock_num = '123456'
     item_class_name = name
     allowed_domains = []
@@ -26,25 +26,33 @@ class FinanceSpider(scrapy.Spider):
     def parse(self, response):
         self.item_class_name = 'test'
         name = response.xpath('//*[@id="in_squote"]/div/h1/a[1]/strong/text()').extract_first()
-        print name
-        benefit = response.xpath('//*[@id="benefit"]/text()').extract_first()
-        debt = response.xpath('//*[@id="debt"]/text()').extract_first()
-        cash = response.xpath('//*[@id="cash"]/text()').extract_first()
-        main = response.xpath('//*[@id="main"]/text()').extract_first()
-        each = response.xpath('//*[@id="each"]/text()').extract_first()
-        operate = response.xpath('//*[@id="operate"]/text()').extract_first()
-        grow = response.xpath('//*[@id="grow"]/text()').extract_first()
-        pay = response.xpath('//*[@id="pay"]/text()').extract_first()
-
         finance = FinanceItem()
         finance["name"] = name
         finance["num"] = response.meta['num']
-        finance["benefit"] = json.loads(benefit)
-        finance["debt"] = json.loads(debt)
-        finance["cash"] = json.loads(cash)
-        finance["main"] = json.loads(main)
-        finance["each"] = json.loads(each)
-        finance["operate"] = json.loads(operate)
-        finance["grow"] = json.loads(grow)
-        finance["pay"] = json.loads(pay)
+
+        self.parse_subject(finance, response, 'benefit')
+        self.parse_subject(finance, response, 'debt')
+        self.parse_subject(finance, response, 'cash')
+        self.parse_subject(finance, response, 'main')
+        self.parse_subject(finance, response, 'each')
+        self.parse_subject(finance, response, 'operate')
+        self.parse_subject(finance, response, 'grow')
+        self.parse_subject(finance, response, 'pay')
+
         yield finance
+
+    def parse_subject(self, finance, response, key='benefit'):
+        data = response.xpath('//*[@id="%s"]/text()' % key).extract_first()
+        json_data = json.loads(data)
+        names = json_data['title']
+        d = json_data['simple']
+        dates = d[0]
+        subjects = []
+        for x in range(len(dates)):
+            # 每列(日期)
+            date = dates[x]
+            for i in range(1, len(names)):
+                # 每行(科目)
+                subject = {'date': date, 'name': names[i][0], 'value': d[i][x]}
+                subjects.append(subject)
+        finance[key] = subjects

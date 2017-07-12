3f884517d0c649fb40a151cd09e0f6a16bcfcf69
nikytwo <nikytwo@gmail.com>
2017-07-12 14:01:44 +0800

    feat: 爬取网站主页网址

--- stockSpider/spiders/company.py
+++ stockSpider/spiders/company.py
@@ -25,11 +25,13 @@ class CompanySpider(BaseSpider):
         vocation = response.xpath('//*[@id="detail"]/div[2]/table/tbody/tr[2]/td[2]/span/text()').extract_first()
         province = response.xpath('//*[@id="detail"]/div[2]/table/tbody/tr[1]/td[3]/span/text()').extract_first()
         start_date = response.xpath('//*[@id="publish"]/div[2]/table/tr[2]/td[1]/span/text()').extract_first()
+        web_home = response.xpath('//*[@id="detail"]/div[2]/table/tbody/tr[3]/td[2]/span/a/text()').extract_first()
 
         company = CompanyItem()
         company['num'] = response.meta['num']
         company['company_name'] = company_name
         company['province'] = province
         company['vocation'] = vocation
+        company['web_home'] = web_home
         company['start_date'] = start_date
         yield company
